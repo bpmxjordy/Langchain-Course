ea369208685ea848cb09579edbe8d47b0fcdbe8c
jordan cartwright <bpmxjordy@gmail.com>
2026-04-26 18:55:10 +0100

    First langchain invoke. Looped over 3 people and used a langchain chain to retrive their DOB, 2 facts and their latest accomplishment.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from dotenv import load_dotenv
 import os
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
@@ -7,7 +9,30 @@ load_dotenv()
 
 def main():
     print("Hello from langchain-course!")
+    information = ["Elon Musk", "Jenson huang", "Donald Trunp"]
 
+    facts = []
 
+    for i in information:
+
+        summary_template = f"""
+        Given the following person: {i} return:
+        1. Date of birth.
+        2. Two interesting facts about the person.
+        3. Their latest accomplishment.
+        """
+
+        summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+
+        llm = ChatOpenAI(model="gpt-5", temperature=0)
+
+        chain = summary_prompt_template | llm
+
+        response = chain.invoke(input={"information": i})
+
+        facts.append([response.content])
+
+    for i in facts:
+        print(i)
 if __name__ == "__main__":
     main()
